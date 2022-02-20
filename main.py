9cad03a7cb67cec06038c21588e1f42fcfc61a4f
DavidKastler <david1kastler@gmail.com>
2022-02-20 19:04:00 +0100

    Update v main funkcii

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import sipFullProxyLib.sipfullproxy as sip_proxy
 import time
-import logging
 import socket
 import sys
 import socketserver
@@ -17,25 +16,23 @@ def set_custom_messages():
     sip_proxy.msg_500 = "Nieco sa dojebalo"
 
 
-def set_up_logging():
-    sip_proxy.logging.basicConfig(format='%(asctime)s:%(levelname)s:%(message)s',
-                                  filename='logs\\proxy.log', filemode='w',
-                                  level=logging.INFO, datefmt='%H:%M:%S')
-    logging.info('\n\tNEW SESSION\n\t' + time.strftime("%a, %d %b %Y %H:%M:%S ", time.localtime()) + '\n')
-
-
 if __name__ == "__main__":
     set_custom_messages()
 
-    set_up_logging()
+    logger = sip_proxy.innit_logging()
 
     hostname = socket.gethostname()
-    logging.info(hostname)
-    ipaddress = socket.gethostbyname(hostname)
 
+    ipaddress = socket.gethostbyname(hostname)
     if ipaddress == "127.0.0.1":
         ipaddress = sys.argv[1]
-    logging.info(ipaddress)
+
+    start_msg = 'NEW SESSION\n\t' + time.strftime("%a, %d %b %Y %H:%M:%S ", time.localtime()) + '\n\t' +\
+                hostname + '\n\t' + ipaddress
+
+    print(start_msg)
+    logger.info('\n\t' + start_msg)
+
     sip_proxy.recordroute = "Record-Route: <sip:%s:%d;lr>" % (ipaddress, PROXY_PORT)
     sip_proxy.topvia = "Via: SIP/2.0/UDP %s:%d" % (ipaddress, PROXY_PORT)
     server = socketserver.UDPServer((HOST, PROXY_PORT), sip_proxy.UDPHandler)
